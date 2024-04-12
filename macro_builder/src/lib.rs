ff8488b21a7988a4461cd85fccf9ad761b139464
dimkar <Dimkar3000@gmail.com>
2024-04-12 08:57:15 +0300

    organised some code

--- macro_builder/src/lib.rs
+++ macro_builder/src/lib.rs
@@ -1,25 +1,11 @@
 
 
-use proc_macro::TokenStream;
+use proc_macro2::{TokenStream, Ident};
 use quote::{format_ident, quote, ToTokens};
-use syn::{parse_macro_input, spanned::Spanned, Attribute, DeriveInput, Meta, MetaList};
+use syn::{parse_macro_input, spanned::Spanned, Attribute, DeriveInput, FieldsNamed, Meta, MetaList};
 
-#[proc_macro_derive(Builder, attributes(expand, variance))]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-    let name = ast.ident;
-
-    let enum_type_name = format!("{name}FieldType");
-    let enum_type_name_ider = syn::Ident::new(&enum_type_name, name.span());
-    
-    let fields = if let syn::Data::Struct(syn::DataStruct{ fields: syn::Fields::Named(ref named, ..), ..}) = ast.data {
-        named
-    } else {
-        unimplemented!()
-    };
-
-    // Enum members
-    let enum_fields = fields.named.iter().map(|x| {
+fn create_enum_field(fields: &FieldsNamed) -> Vec<TokenStream> {
+    fields.named.iter().map(|x| {
         let name = format!("{}", x.ident.as_ref().unwrap());
         let first_letter = name.get(..1).unwrap().to_ascii_uppercase();
         let field_name = format!("{}{}", first_letter, name.get(1..).unwrap());
@@ -34,10 +20,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
         quote! {
             #name_idt(#inner)
         }
-    });
+    }).collect()
+}
 
-    // apply match arguments
-    let apply_matches = fields.named.iter().map(|x| {
+fn create_apply_matches(fields: &FieldsNamed, enum_idt: &Ident) -> Vec<TokenStream> {
+    fields.named.iter().map(|x| {
         let member_name = format_ident!("{}", x.ident.as_ref().unwrap());
 
         let name = format!("{}", x.ident.as_ref().unwrap());
@@ -46,23 +33,59 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let name_idt = syn::Ident::new(&field_name, x.span());
         if x.attrs.len() > 0 {
             return quote! {
-                #enum_type_name_ider::#name_idt(x) => self.#member_name.apply(x)
+                #enum_idt::#name_idt(x) => self.#member_name.apply(x)
             }
             }
         quote! {
-            #enum_type_name_ider::#name_idt(x) => self.#member_name = x
+            #enum_idt::#name_idt(x) => self.#member_name = x
         }
-    });
+    }).collect()
+}
+
+fn get_variance(attrs: &[Attribute]) -> Option<syn::LitInt> {
+    for attr in attrs {
+        if let Meta::List(MetaList{path, tokens, ..}) = &attr.meta {
+            let identifier = path.segments.first();
+            if identifier.is_none() {
+                continue;
+            }
+            let identifier = identifier.unwrap();
+            if identifier.ident.to_string() != "variance" {
+                continue;
+            }
+            let result = syn::LitInt::new(&tokens.to_string(), attr.span());
+            return Some(result)
+        } else {
+            continue
+        };
+    }
+    None
+}
+
+#[proc_macro_derive(Builder, attributes(expand, variance))]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("ast: {ast:#?}");
+    let name = ast.ident;
+
+    let enum_type_idt = format_ident!("{name}FieldType");
+    
+    let fields = if let syn::Data::Struct(syn::DataStruct{ fields: syn::Fields::Named(ref named, ..), ..}) = ast.data {
+        named
+    } else {
+        unimplemented!()
+    };
+
+    // Enum members
+    let enum_fields = create_enum_field(fields);
+
+    // apply match arguments
+    let apply_matches = create_apply_matches(fields, &enum_type_idt);
 
     // variant size constant
     let contant_name = format!("{}_VARIANT_SIZE", name).to_ascii_uppercase();
     let size_const_name = syn::Ident::new(&contant_name, name.span());
-    let count = if let Some(Attribute { meta: Meta::List(MetaList{ tokens, ..}),.. }) = ast.attrs.first() {
-        tokens.to_string()
-    } else {
-        unimplemented!()
-    };
-    let count_const = syn::LitInt::new(&count.to_string(), fields.span());
+    let constant_literal = get_variance(&ast.attrs).unwrap();
     
     // setters
     let setters_name = format_ident!("{}Setters", name);
@@ -103,7 +126,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             return quote! {
                 fn #member_name(&mut self) -> impl #setter_idt {
                     let f = self.1;
-                    SettersImpl(&mut self.0[1..(#count_const)], move |x| f(#enum_type_name_ider::#enum_name_idt(x)))
+                    SettersImpl(&mut self.0[1..(#constant_literal)], move |x| f(#enum_type_idt::#enum_name_idt(x)))
                 }
             }
     
@@ -111,7 +134,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         quote! {
             fn #member_name<'b>(&'b mut self) -> impl FieldSetter<#s> + 'b {
                 let f = self.1;
-                FieldSetterImpl::<_, T, #index, _>(self.0, move |x| f(#enum_type_name_ider::#enum_name_idt(x)), PhantomData)
+                FieldSetterImpl::<_, T, #index, _>(self.0, move |x| f(#enum_type_idt::#enum_name_idt(x)), PhantomData)
             }
         }
     });
@@ -122,29 +145,29 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     quote! {
         #[derive(Debug, PartialEq, Eq)]
-        pub enum #enum_type_name_ider  {
+        pub enum #enum_type_idt  {
             #(#enum_fields),*
         }
 
         impl #name {
-            pub fn apply(&mut self, field: #enum_type_name_ider) {
+            pub fn apply(&mut self, field: #enum_type_idt) {
                 match field {
                     #(#apply_matches),*
                 }
             }
         }
 
-        pub const #size_const_name: usize = #count_const;
+        pub const #size_const_name: usize = #constant_literal;
 
         pub trait #setters_name {
             #(#setter_members)*
         }
 
-        impl<'a, T, F: Fn(#enum_type_name_ider) -> T + Copy> #setters_name for SettersImpl<'a, T, F> {
+        impl<'a, T, F: Fn(#enum_type_idt) -> T + Copy> #setters_name for SettersImpl<'a, T, F> {
             #(#setter_accessors)*
         }
 
-        pub struct #observer_name([Option<#enum_type_name_ider>; #size_const_name]);
+        pub struct #observer_name([Option<#enum_type_idt>; #size_const_name]);
 
         impl #observer_name {
             pub fn new() -> Self {
@@ -155,7 +178,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 SettersImpl(&mut self.0, Some)
             }
 
-            pub fn events(&self) -> impl Iterator<Item = &#enum_type_name_ider> {
+            pub fn events(&self) -> impl Iterator<Item = &#enum_type_idt> {
                 self.0.iter().flatten()
             }
 
@@ -165,8 +188,3 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     }.into()
 }
-
-// #[proc_macro_attribute]
-// pub fn exp(_: TokenStream, _item: TokenStream) -> TokenStream {
-//     TokenStream::new()
-// }
