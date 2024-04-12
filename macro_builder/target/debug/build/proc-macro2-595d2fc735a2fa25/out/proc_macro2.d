e38df0c6e200090653afd9fc17e769cd4abb0d6e
dimkar <Dimkar3000@gmail.com>
2024-04-12 09:19:27 +0300

    formatting and ignore file changes

--- macro_builder/target/debug/build/proc-macro2-595d2fc735a2fa25/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-C:\Repositories\test_events\macro_builder\target\debug\build\proc-macro2-595d2fc735a2fa25\out\libproc_macro2.rmeta: build\probe.rs
-
-C:\Repositories\test_events\macro_builder\target\debug\build\proc-macro2-595d2fc735a2fa25\out\proc_macro2.d: build\probe.rs
-
-build\probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- macro_builder/target/debug/build/proc-macro2-f39e1886de376986/build_script_build-f39e1886de376986.d
+++
@@ -1,5 +0,0 @@
-C:\Repositories\test_events\macro_builder\target\debug\build\proc-macro2-f39e1886de376986\build_script_build-f39e1886de376986.exe: C:\Users\dimkar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.79\build.rs
-
-C:\Repositories\test_events\macro_builder\target\debug\build\proc-macro2-f39e1886de376986\build_script_build-f39e1886de376986.d: C:\Users\dimkar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.79\build.rs
-
-C:\Users\dimkar\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.79\build.rs:
